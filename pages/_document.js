01f32b3b6cee3e585b0ab57bd374b3a497a0d1a0
Victoria <vmcnorrill@gmail.com>
2024-01-29 12:50:52 -0500

    added personal images and photo. configured personal details

--- pages/_document.js
+++ pages/_document.js
@@ -2,8 +2,8 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   const meta = {
-    title: 'Next.js Blog Starter Kit',
-    description: 'Clone and deploy your own Next.js portfolio in minutes.',
+    title: 'Victoria McNorrill | Software Developer',
+    description: 'Victoria McNorrill is  a full-stack software developer. victoriamcodes.com is her portfolio website',
     image: 'https://assets.vercel.com/image/upload/q_auto/front/vercel/dps.png'
   }
 
--- scripts/gen-rss.js
+++ scripts/gen-rss.js
@@ -5,9 +5,9 @@ const matter = require('gray-matter')
 
 async function generate() {
   const feed = new RSS({
-    title: 'Your Name',
-    site_url: 'https://yoursite.com',
-    feed_url: 'https://yoursite.com/feed.xml'
+    title: 'Victoria McNorrill',
+    site_url: 'https://victoriamcodes.com',
+    feed_url: 'https://victoriamcodes.com/feed.xml'
   })
 
   const posts = await fs.readdir(path.join(__dirname, '..', 'pages', 'posts'))
